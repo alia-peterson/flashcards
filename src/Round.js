63dcc959768bb32b6621e59aef47fd0f5691668e
Alia Peterson <alia@pnwebs.com>
2020-11-30 22:54:06 -0700

    Add additional round components and tests

--- src/Round.js
+++ src/Round.js
@@ -1,14 +1,20 @@
 class Round {
-  constructor() {
-
+  constructor( deck ) {
+    this.deck = deck
+    this.turns = 0
+    this.incorrectGuesses = [] // stores the id of the card that was guessed wrong
   }
 
   returnCurrentCard() {
-
+    // return card being played
   }
 
   takeTurn() {
-
+    // new turn instance is created
+    // turns count is updated
+    // the next card becomes the current card
+    // the guess is evaluated/recorded and incorrect guesses are stored
+    // feedback is returned based on answer given
   }
 
   calculatePercentCorrect() {
@@ -16,7 +22,7 @@ class Round {
   }
 
   endRound() {
-    
+    // prints round over and percent correct message
   }
 }
 
--- test/Round-test.js
+++ test/Round-test.js
@@ -15,4 +15,42 @@ describe('Round', function() {
     expect(round).to.be.an.instanceof(Round)
   })
 
+  it.skip('should store the deck of cards', function() {
+    const card1 = new Card(1, 'Question1', ['close', 'correct', 'wrong'], 'Correct')
+    const card2 = new Card(4, 'Question2', ['a', 'b', 'c'], 'c')
+    const card3 = new Card(6, 'Question3', ['cat', 'dog', 'horse'], 'cat')
+    const deck = new Deck([card1, card2, card3])
+    const round = new Round(deck)
+
+    expect(round.deck).to.deep.equal([card1, card2, card3])
+  })
+
+  it.skip('should return the current card', function() {
+
+  })
+
+  it.skip('should create a new turn instance', function() {
+    // take turn
+  })
+
+  it.skip('should increase the turn counter', function() {
+
+  })
+
+  it.skip('should update the current card', function() {
+
+  })
+
+  it.skip('should store the incorrect guesses', function() {
+
+  })
+
+  it.skip('should provide feedback based on answer', function() {
+
+  })
+
+  it.skip('should calculate the percent of correct answers', function() {
+    // should also print game over message
+  })
+
 })
