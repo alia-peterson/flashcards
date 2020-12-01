48698463821015009219d2aa52a90fba555a1b51
Alia Peterson <alia@pnwebs.com>
2020-11-30 22:12:18 -0700

    Create initial deck tests

---
+++ test/Deck-test.js
@@ -0,0 +1,24 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Deck = require('../src/Deck');
+
+describe('Deck', function() {
+
+  it.skip('should be a function', function() {
+
+  })
+
+  it.skip('should be an instance of Deck', function() {
+
+  })
+
+  it.skip('should be an array', function() {
+
+  })
+
+  it.skip('should store the correct number of cards', function() {
+    
+  })
+
+}
