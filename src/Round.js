3cbd6838f7e067935608319b501d1b60f548b2f7
Alia Peterson <alia@pnwebs.com>
2020-12-01 18:46:28 -0700

    Revise round methods

--- src/Round.js
+++ src/Round.js
@@ -1,20 +1,27 @@
+const Turn = require('../src/Turn')
+
 class Round {
   constructor( deck ) {
     this.deck = deck
     this.turns = 0
-    this.incorrectGuesses = [] // stores the id of the card that was guessed wrong
+    this.currentCard = deck.cards[0]
+    this.incorrectGuesses = []
   }
 
   returnCurrentCard() {
-    // return card being played
+    return this.currentCard
   }
 
-  takeTurn() {
-    // new turn instance is created
-    // turns count is updated
-    // the next card becomes the current card
-    // the guess is evaluated/recorded and incorrect guesses are stored
-    // feedback is returned based on answer given
+  takeTurn( playerGuess ) {
+    const turn = new Turn( playerGuess, this.currentCard )
+    if (!turn.evaluateGuess()) {
+      this.incorrectGuesses.push(this.currentCard.id)
+    }
+    const feedback = turn.giveFeedback()
+    this.turns += 1
+    this.currentCard = this.deck.cards[this.turns]
+
+    return feedback
   }
 
   calculatePercentCorrect() {
