4c01da95583f42ff755891d7d0962c2db33a2609
Alia Peterson <alia@pnwebs.com>
2020-12-02 13:25:14 -0700

    Add game.start logic and necessary variables

--- src/Game.js
+++ src/Game.js
@@ -1,18 +1,32 @@
-const data = require('./data');
-const prototypeQuestions = data.prototypeData;
-const util = require('./util');
+const data = require('./data')
+const prototypeQuestions = data.prototypeData
+const util = require('./util')
+const Card = require('./Card')
+const Round = require('./Round')
+const Deck = require('./Deck')
 
 class Game {
   constructor() {}
 
-  printMessage(deck, round) {
-      console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
+  printMessage( deck ) {
+    console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
   }
 
-  printQuestion(round) {
-      util.main(round);
+  printQuestion( round ) {
+    util.main( round )
+  }
+
+  start() {
+    const deck = new Deck()
+    prototypeQuestions.forEach(( cardData ) => {
+      let card = new Card(cardData.id, cardData.question, cardData.answers, cardData.correctAnswer)
+      deck.cards.push( card )
+    })
+    const round = new Round( deck )
+    this.printMessage( deck )
+    this.printQuestion( round )
   }
 }
 
-module.exports = Game;
+module.exports = Game
