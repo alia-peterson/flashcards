824b1781307fb9a7464d3e3a499a25e932e5f3d1
Alia Peterson <alia@pnwebs.com>
2020-12-01 17:08:16 -0700

    Refactor to include beforeEach

--- test/Round-test.js
+++ test/Round-test.js
@@ -1,36 +1,41 @@
 const chai = require('chai')
 const expect = chai.expect
 
+const Turn = require('../src/Turn')
+const Card = require('../src/Card')
+const Deck = require('../src/Deck')
 const Round = require('../src/Round')
 
 describe('Round', function() {
+  let card1, card2, card3, deck, currentRound
+
+  beforeEach(function() {
+    card1 = new Card(1, 'Question1', ['close', 'correct', 'wrong'], 'correct')
+    card2 = new Card(4, 'Question2', ['a', 'b', 'c'], 'c')
+    card3 = new Card(6, 'Question3', ['cat', 'dog', 'horse'], 'cat')
+    deck = new Deck([card1, card2, card3])
+    round = new Round(deck)
+  })
 
   it('should be a function', function() {
-    const round = new Round()
     expect(Round).to.be.a('function')
   })
 
   it('should be an instance of Round', function() {
-    const round = new Round()
     expect(round).to.be.an.instanceof(Round)
   })
 
-  it.skip('should store the deck of cards', function() {
-    const card1 = new Card(1, 'Question1', ['close', 'correct', 'wrong'], 'Correct')
-    const card2 = new Card(4, 'Question2', ['a', 'b', 'c'], 'c')
-    const card3 = new Card(6, 'Question3', ['cat', 'dog', 'horse'], 'cat')
-    const deck = new Deck([card1, card2, card3])
-    const round = new Round(deck)
-
-    expect(round.deck).to.deep.equal([card1, card2, card3])
+  it('should store the deck of cards', function() {
+    expect(round.deck).to.deep.equal(deck)
   })
 
-  it.skip('should return the current card', function() {
-
+  it('should return the current card', function() {
+    expect(round.returnCurrentCard()).to.equal(card1)
   })
 
-  it.skip('should create a new turn instance', function() {
-    // take turn
+  it('should create a new turn instance', function() {
+    round.takeTurn('correct')
+    expect(turn).to.be.an.instanceof(Turn)
   })
 
   it.skip('should increase the turn counter', function() {
