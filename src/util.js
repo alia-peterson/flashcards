c21ec31fcbfc4d21ee493203e968d9009893e012
Alia Peterson <alia@pnwebs.com>
2020-12-03 19:09:27 -0700

    Add endgame prompt and additional supporting functions

--- src/util.js
+++ src/util.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer')
+// const game = require('./index')
 
 const genList = (round) => {
   let card = round.returnCurrentCard()
@@ -29,14 +30,47 @@ const confirmUpdate = (id, round) => {
   }
 }
 
-async function main(round) {
+const confirmGame = {
+  type: 'rawlist',
+  message: 'What would you like to do?',
+  name: 'answers',
+  choices: [
+    {
+      key: 'id1',
+      value: 'Try Again'
+    },
+    {
+      key: 'id2',
+      value: 'Exit Game'
+    }
+  ]
+}
+
+async function continueGame() {
+  return await inquirer.prompt(confirmGame)
+}
+
+async function main(round, game) {
 
   const currentRound = await getRound(round)
   const getAnswer = await inquirer.prompt(genList(currentRound))
   const getConfirm = await inquirer.prompt(confirmUpdate(getAnswer.answers, round))
 
-    if(!round.returnCurrentCard()) {
+    if( !round.returnCurrentCard() ) {
       round.endRound()
+
+      if ( round.incorrectGuesses.length === 0 ) {
+        process.exit()
+      }
+
+      continueGame().then( result => {
+
+        if (result.answers === 'Try Again') {
+          game.start(round.incorrectGuesses)
+        } else {
+          process.exit()
+        }
+      })
     } else {
       main(round)
     }
